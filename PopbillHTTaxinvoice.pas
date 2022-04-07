b9e48659c5d27bedd6d6c4d9277f7a3d1f2f0a7d
WonJae <wjkim@linkhub.co.kr>
2022-04-07 17:26:07 +0900

    remove 개인메일

--- PopbillHTTaxinvoice.pas
+++ PopbillHTTaxinvoice.pas
@@ -7,7 +7,7 @@
 * http://www.popbill.com
 * Author : Jeong Yohan (code@linkhubcorp.com)
 * Written : 2016-06-10
-* Updated : 2022-01-10
+* Updated : 2022-04-07
 * Thanks for your interest. 
 *=================================================================================
 *)
